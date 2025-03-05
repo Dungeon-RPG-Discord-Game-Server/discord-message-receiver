05af454238371c315b808af48e2fb102c3c5950d
d.lee <d.lee@digipen.edu>
2025-03-05 14:54:08 -0800

    .env 파일 이슈 수정정

--- DiscordMessageReceiver/Program.cs
+++ DiscordMessageReceiver/Program.cs
@@ -18,7 +18,8 @@ namespace DiscordMessageReceiver
         public async Task MainAsync()
         {
             _clientManger = new DiscordClientManager();
-
+            var envPath = "";
+            Env.Load(envPath);
             // 환경 변수나 별도의 설정 파일에서 토큰을 불러오는 것을 권장합니다.
             Console.WriteLine("현재 작업 디렉터리: " + Environment.CurrentDirectory);
             string? token = Environment.GetEnvironmentVariable("BOT_TOKEN");
